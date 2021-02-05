32b48adbacc22bad5c17c70a8501b91ef174b9d1
ReidBarbeln2022 <814684@mystma.org>
2021-02-05 15:59:40 +0000

    length of three try 1

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def length3(x, y, z):
     pop = 0
-    pop = len(x) + len(y) + len(x)
-    return pop
+    pop = len(x) + len(y) + len(z)
+    return pop 
 
 
-print (length3("aaa", "aaa", "aaa"))
+print (length3("cat", "dog", "rat"))
